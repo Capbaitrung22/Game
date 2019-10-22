8868f645275e02429ed7d2a66af6152d76fdad26
TrungThanhNhan <capbaitrung22@gmail.com>
2019-10-22 15:21:57 +0700

    Game

---
+++ ConsoleApplication1/ConsoleApplication1/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Controller
+    {
+        private Board Game;
+        private Player P1, P2;
+       public  Controller()
+        {
+            P1 = new Player("Hung");
+            P2 = new Player("Nhan");
+            Game = new Board(3, 4, 6);
+        }
+        
+        public void PlayGame()
+        {
+            Game.PrintGame();
+            while (true) 
+            {
+                Console.WriteLine(P1.GetName() + "a turn");
+                P1.PickBall(Game);
+                Game.PrintGame();
+                if(Game.IsGameOver())
+                {
+                    Console.WriteLine(P1.GetName() + " Hung da thua");
+                    Console.WriteLine(P2.GetName() + " Nhan da thang");
+                    break;
+                }
+                Console.WriteLine(P2.GetName() + " a turn");
+                P2.PickBall(Game);
+                Game.PrintGame();
+                if(Game.IsGameOver())
+                {
+                    Console.WriteLine(P2.GetName() + " Nhan da thua ");
+                    Console.WriteLine(P1.GetName() + "Hung da thang ");
+                    break;
+                }
+            }
+        }
+
+    }
+}
